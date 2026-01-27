11642b678d3c7b1dae9104ecb395492ee7639271
MrMuto <abid.koussay18@gmail.com>
2026-01-27 21:27:47 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -24,3 +24,26 @@ self.addEventListener('fetch', event => {
       .then(response => response || fetch(event.request))
   );
 });
+self.addEventListener('push', function(event) {
+    const data = event.data ? event.data.json() : { title: 'تنبيه جديد', body: 'تفقد تطبيق فلاحين!' };
+    
+    const options = {
+        body: data.body,
+        icon: '/images/icon-192.png',
+        badge: '/images/icon-192.png',
+        vibrate: [100, 50, 100],
+        data: { url: '/' }
+    };
+
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});
+
+// كي ينزل الفلاح على التنبيه، يتحل الموقع
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow('/')
+    );
+});
